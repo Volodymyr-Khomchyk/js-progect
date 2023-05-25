978c7c9663c1594e00375c57b3106540e2e5bf74
Volodymyr Khomchyk <khomchikvv@gmail.com>
2023-05-25 19:14:13 +0200

    Type Zminni

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-console.log(true);// boolean
-console.log('This is text');
-console.log(20);
-console.log(undefined);
-console.log(null);
-console.log(5n);
+// Variable Змінна
+// Створення змінної
+// ключове слово < > ім'я змінної
+// var -- deprecated
+// let -- CD-RW
+// const -- CD-R
+
+let box; // Variable Declaration Оголошення змінної
+// оператор присвоєння
+box=5
+console.log(box);
+
+
+let box2 = "Hello";
+console.log(box2);
+
+box2 = '10';
+console.log(box2);
+
+const isActive = false;
+const isLogged = true;
+
+const x = 10+2;
+let y = x;
+console.log(y);
