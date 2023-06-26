1428b521de36f9a3390fc699b5229cc2543ff754
Volodymyr Khomchyk <khomchikvv@gmail.com>
2023-06-26 19:14:23 +0200

    Botton

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-// function addFiveToNumber(number){
-//     let result = number + 5;
-//     console.log(result);
-// }
+// console.log(this);
 
-// addFiveToNumber(15);
+// alert();
 
-//--------------------------------
+console.dir(document);
 
-// const greet = function(name){
-//     console.log('Привіт, ' + name);
-// }
-
-// greet('John');
-
-
-//--------------------------------
-
-// const greetArrow = (name) => {
-//     console.log('Привіт, ' + name);
-// }
-
-// greetArrow('Mike');
-
-let num1 = Number(prompt('Введіть перше число'));
-let num2 = Number(prompt('Введіть перше число'));
-
-function addTwoNumbers(arg1, arg2){
-    const result = arg1 + arg2;
-    return result;
+function getAlert(event){
+    const btn = event.target;
+    // btn.style.backgraundColor = 'red';
+    if (btn.style.backgroundColor === 'red'){
+        btn.style.backgroundColor = 'unset';
+    }else {
+        btn.style.backgroundColor = 'red';
+    }
 }
 
-let result = addTwoNumbers(num1, num2);
-console.log(result);
+function consolHello(event){
+    console.log('Hello!!!');
+}
+
+
+  
+const buttonElement = document.getElementById('btn');
+const secondButton = document.getElementById('second-btn');
+
+console.log(buttonElement);
+console.dir(buttonElement);
+
+buttonElement.addEventListener('click', getAlert);
+secondButton.addEventListener('mouseover', consolHello);
